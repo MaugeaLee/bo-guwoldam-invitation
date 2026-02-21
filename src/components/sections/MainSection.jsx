ed4a880c55739cef8057bddf44c4c1e716914851
MaugeaLee <dlqhgur@naver.com>
2026-02-22 00:32:17 +0900

    update(MainSection): MainSection의 화면 구성 정의

--- src/components/sections/MainSection.jsx
+++ src/components/sections/MainSection.jsx
@@ -1,23 +1,39 @@
 import React, { useState } from 'react';
-import {Container, Typography, Button} from '@mui/material';
+import {Container, Typography, Button, Box} from '@mui/material';
+
+import TitleImg from '../../assets/logo.svg';
 
 const MainSection = () => {
     return (
-        <Container maxWidth="sm" sx={{ textAlign: 'center', py: 10 }}>
-            <Typography variant="overline" color="primary">
-                SAVE THE DATE
-            </Typography>
-            <Typography variant="h3" sx={{ my: 2, fontWeight: 'bold' }}>
-                구월담
-            </Typography>
-            <Typography variant="body1" sx={{ mb: 4 }}>
-                2026년 5월 23일 토요일 오후 12시<br />
-                동도로동동 합주실
-            </Typography>
-            <Button variant="contained" disableElevation>
-                구글 캘린더 추가
-            </Button>
-      </Container>
+        <Box>
+            <Container maxWidth="sm" sx={{ textAlign: 'center', py: 10 }}>
+                <Typography variant="overline" color="primary">
+                    SAVE THE DATE
+                </Typography>
+                <Box sx={{
+                    width: '100%',
+                    height: '30vh',
+                    backgroundImage: `url(${TitleImg})`,
+
+                    backgroundRepeat: 'no-repeat', // 반복 금지
+                    backgroundSize: 'contain', // 전체 이미지가 다 보이도록
+                    backgroundPosition: 'center', // 중앙 정렬
+                    
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}/>
+                
+                <Typography variant="body1" sx={{ mb: 4, mt:4 }}>
+                    2026년 5월 23일 토요일 오후 12시<br />
+                    동도로동동 합주실
+                </Typography>
+                <Button variant="contained" disableElevation>
+                    구글 캘린더 추가
+                </Button>
+        </Container>
+      </Box>
     )
 }
 
