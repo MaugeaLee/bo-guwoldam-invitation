d9c7d4d13bea9fd91b6f75b872efb032d4678bf2
MaugeaLee <dlqhgur@naver.com>
2026-02-24 16:56:15 +0900

    Create(Section): 공용 Layout용 섹션 컴포넌트 생성

---
+++ src/components/Section.jsx
@@ -0,0 +1,27 @@
+import { Box, Container } from '@mui/material';
+
+
+const Section = ({ children, sx = {} }) => {
+    return (
+        <Box
+          sx={{
+            minHeight: '100dvh',
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+
+            // 공통 패딩
+            px: 3,
+            py: 6,
+            boxSizing: 'border-box',
+            position: 'relative',
+          }}>
+            <Container maxWidth='sm'>
+                {children}
+            </Container>
+        </Box>
+    );
+};
+
+export default Section;
